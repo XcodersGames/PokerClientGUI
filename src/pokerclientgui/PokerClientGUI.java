6a282a58707f43be078f39b82b0fb14ab5cbcc0c
Vijani <vijanipiyawardana@gmail.com>
2014-07-30 14:06:06 +0530

    Register Button Click Event

---
+++ src/pokerclientgui/PokerClientGUI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokerclientgui;
+
+import com.xcoders.view.LogoScreen;
+
+/**
+ *
+ * @author ravindu
+ */
+public class PokerClientGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new LogoScreen().setVisible(true);
+    }
+}
